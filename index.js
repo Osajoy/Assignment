3edb2d1648d4c49a61aecd4e7005bd45ff1efe9c
Osajoy <joyikponmwosa245@gmail.com>
2022-10-31 20:59:27 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+prompt("what is your name?");
+prompt("what is their name?");
+var lovescore = Math.random() * 100;
+lovescore = Math.floor(lovescore) +1;//1-100
+if (lovescore > 60){
+    
+    alert("your lovescore is"+ lovescore +"%" + "you love each other like kanye loves kanye");
+if (lovescore >30 && lovescore <=60);
+alert("your love score is" + lovescore +"%");
+     
+} else {
+    alert("your love score is" + lovescore +"%" +"you go together like oil and water");
+}
+
